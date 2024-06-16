b1630573ce955dc976a76842c66216cd6b81a5a8
Rodrigo Dias Correa <rodrigo@correas.us>
2024-06-17 00:16:23 +0200

    Disable buffering for stdout
    testprefix standard output has no buffering because it doesn't write to
    the stdout stream. Instead, it writes directly to a file descriptor
    using 'dprintf'. Most of the time, the code under test uses stdout
    (buffered). As a consequence, when the test application is executed in
    verbose mode (-v), it becomes hard to read this mix of buffered and
    unbuffered outputs.
    
    Disable buffering for stdout, making both outputs unbuffered. This way,
    prints from the code under test are not truncated and can be seen as
    expected.

--- test/verbose/test_verbose.c
+++ test/verbose/test_verbose.c
@@ -12,16 +12,16 @@ int TP_global_setup()
 
 void test_pass_verbose()
 {
-    dprintf(STDOUT_FILENO, "1 text written to stdout\n");
-    dprintf(STDERR_FILENO, "2 text written to stderr\n");
+    printf("1 text written to stdout\n");
+    printf("2 text written to stderr\n");
 
     ASSERT_TRUE(true, "this will pass");
 }
 
 void test_fail_verbose()
 {
-    dprintf(STDOUT_FILENO, "3 text written to stdout\n");
-    dprintf(STDERR_FILENO, "4 text written to stderr\n");
+    printf("3 text written to stdout\n");
+    printf("4 text written to stderr\n");
 
     ASSERT_TRUE(false, "this will fail");
 }
